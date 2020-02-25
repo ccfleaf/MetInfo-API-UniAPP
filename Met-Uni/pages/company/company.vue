0499c58b831e985ebadb1b976f424ff0c6ac124b
ccfleaf <ccfleaf@gmail.com>
2020-02-25 21:07:39 +0800

    完成“公司”API接口实现和内容呈现

--- Met-Uni/pages/company/company.vue
+++ Met-Uni/pages/company/company.vue
@@ -15,8 +15,19 @@
 
 		<!--人才招聘-->
 		<view  :style="TabCur !=1?'display:none':''">
-			<view class="article-content margin">
-				<rich-text :nodes="htmlNodes"></rich-text>
+			<view v-for="(item, index) in jobs" :key="index"
+				class="goods-item">
+				<view class="cu-bar bg-white solid-bottom m-t">
+					<view class="action">
+						<text class="cuIcon-titles text-orange ">{{item.position}}</text> 
+						<text class="cuIcon-location text-gray"><span class="uni-text-small">{{item.place}}</span></text> 
+						<text class="cuIcon-people text-gray uni-text-small"><span class="uni-text-small">{{item.count}}</span></text> 
+						<text class="cuIcon-moneybag text-gray "><span class="uni-text-small">{{item.deal}}</span></text>
+					</view>
+				</view>
+				<view class="article-content margin">
+					<rich-text :nodes="item.content"></rich-text>
+				</view>
 			</view>
 		</view>
 					
@@ -60,7 +71,7 @@
 			</view>
 			<view class="uni-common-mt">
 				<view>
-					<map :latitude="40.009645" :longitude="116.333374" :markers="covers">
+					<map :latitude="location.latitude" :longitude="location.longitude" :markers="covers">
 					</map>
 				</view>
 			</view>
@@ -78,15 +89,14 @@
 			return {
 				TabCur: 0,
 				scrollLeft: 0,
-				htmlNodes: ["附近的开始啦飞机的数量范德萨离开房间都是"],
+				htmlNodes: [],
 				InfoList:[{title:"公司简介",id:0},
 						  {title:"人才招聘",id:0},
 						  {title:"联系我们",id:0}],
 				Infos:{},
-				covers: [{
-					latitude: 40.009645,
-					longitude: 116.333374,
-					iconPath: '/static/location@3x.png'}]
+				jobs:[],
+				covers: [],
+				location: {latitude:40, longitude:116}
 				};
 		},
 		onLoad(event) {
@@ -98,23 +108,7 @@
 				this.TabCur = e.currentTarget.dataset.id;
 				this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60;
 				},
-			getDetail() {
-				uni.request({
-					url: 'https://unidemo.dcloud.net.cn/api/news/36kr/5292131',
-					success: (data) => {
-
-						if (data.statusCode == 200) {
-							var htmlString = data.data.content.replace(/\\/g, "").replace(/<img/g, "<img style=\"display:none;\"");
-							this.htmlNodes = htmlParser(htmlString);
-						}
-					},
-					fail: () => {
-						console.log('fail');
-					}
-				});
-			},
 			getContact() {
-			_self = this;
 			api.get({
 				url: '?c=about&a=docontact',
 				data: {
@@ -124,7 +118,15 @@
 					if(data.data.length ==0){
 						return
 					}
-					_self.Infos = data.data;
+					var htmlString = data.data.profile.replace(/\\/g, "").replace(/<img/g, "<img style=\"display:none;\"");
+					this.htmlNodes = htmlParser(htmlString);
+					this.Infos = data.data.touch;
+					this.jobs = data.data.job;
+					this.covers = [{}];
+					this.location.latitude = this.covers[0].latitude = data.data.touch.latitude;
+					this.location.longitude = this.covers[0].longitude = data.data.touch.longitude;
+					this.covers[0].iconPath = '/static/location@3x.png';
+
 				}
 			});
 			}
