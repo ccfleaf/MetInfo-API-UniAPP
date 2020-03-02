582483ec0a008de75358e0c958f446b33ebc5ce8
ccfleaf <ccfleaf@gmail.com>
2020-03-02 21:57:54 +0800

    更新新闻列表、置顶、详情页API

--- MetInfo-Api/api/class/news.class.php
+++ MetInfo-Api/api/class/news.class.php
@@ -12,58 +12,52 @@ class news extends api {
 		parent::__construct();
 	}
 
-	public function showpage($module) {
-		global $_M;
-		$data = load::sys_class('label', 'new')->get($module)->get_one_list_contents($_M['form']['id']);
-		$data['updatetime'] = date($_M['config']['met_contenttime'], strtotime($data['original_updatetime']));
-		$data['addtime'] = date($_M['config']['met_contenttime'], strtotime($data['original_addtime']));
-		$this->check($data['access']);
-		$this->add_array_input($data);
-		$classnow = $data['class3'] ? $data['class3'] :($data['class2'] ? $data['class2'] : $data['class1']);
-		$this->input_class($classnow);
-		$this->seo($data['title'], $data['keywords'], $data['description']);
-		$this->seo_title($data['ctitle']);
-		$this->success("success",$data);
+	public function donewsid(){
+	    global $_M;
+	    
+	    $query = "SELECT id,title,content,concat('http://".$_SERVER['HTTP_HOST']."/',imgurl) as imgurl,updatetime,publisher FROM {$_M['table']['news']} ";
+	    $where = "WHERE lang = 'cn' and id=".$_M['form']['id'];
+
+	    
+	    $query .= $where;
+	    $ret = DB::get_one($query);
+	    
+	    $this->success("success",$ret);
 	}
-
-	public function listpage($module) {
+	
+  public function donewslist() {
 		global $_M;
-		if($_M['form']['pseudo_jump'] && $_M['form']['list']!=1){
-			if(!is_numeric($_M['form']['metid'])){
-				$custom = load::sys_class('label', 'new')->get($module)->database->get_list_by_filename($_M['form']['metid']);
-				$_M['form']['metid'] = $custom['0']['id'];
-			}
-			$_M['form']['id'] = $_M['form']['metid'];
-			return 'show';
-		}
 
-		$classnow = $_M['form']['class3'] ? $_M['form']['class3'] :($_M['form']['class2'] ? $_M['form']['class2'] : $_M['form']['class1']);
-		$classnow = $classnow ? $classnow : $_M['form']['metid'];
-		if(!is_numeric($classnow)){
-			$custom = load::sys_class('label', 'new')->get('column')->get_column_folder($_M['form']['metid']);
-			$classnow = $custom['0']['id'];
+		$query = "SELECT id,title,content,concat('http://".$_SERVER['HTTP_HOST']."/',imgurl) as imgurl,updatetime,publisher FROM {$_M['table']['news']} ";
+		$where = "WHERE top_ok<>1 and lang = 'cn'";
+		$where .= $_M['form']['minId']?" AND id<{$_M['form']['minId']}":'';
+		$pagesize = $_M['form']['pageSize']?$_M['form']['pageSize']:10;
+		$limit =" LIMIT ".$pagesize;
+		$order = " ORDER BY id DESC";
+		
+		$query .= $where.$order.$limit;
+		$ret = DB::get_all($query);
+		/*
+		foreach ($ret as $k => $v) {
+		    $jobs[] = array('position'=>$v['position'],'count'=>$v['count'],'place'=>$v['place'],'deal'=>$v['deal'],'content'=>$v['content']);
 		}
-		$classnow = $this->input_class($classnow);
-		$data = load::sys_class('label', 'new')->get('column')->get_column_id($classnow);
-		$this->check($data['access']);
-		unset($data['id']);
-		$this->add_array_input($data);
-		$this->seo($data['name'], $data['keywords'], $data['description']);
-		$this->seo_title($data['ctitle']);
-		$this->add_input('page', $_M['form']['page']);
-		$this->add_input('list', 1);
-		return 'list';
-	}
-
-  public function donews() {
-		global $_M;
-		$this->success('success',$_M);
+		$infos['job'] = $jobs;
+		*/
+		$this->success("success",$ret);
+		
   }
 
-	public function doshownews(){
-		global $_M;
-		$this->showpage('news');
-		require_once $this->template('tem/shownews');
+	public function donewsbanner(){
+	    global $_M;
+	    
+	    $query = "SELECT id,title,content,concat('http://".$_SERVER['HTTP_HOST']."/',imgurl) as imgurl,updatetime,publisher FROM {$_M['table']['news']} ";
+	    $where = "WHERE top_ok=1 and lang = 'cn'";
+	    $order = " ORDER BY id DESC";
+	    
+	    $query .= $where.$order;
+	    $ret = DB::get_one($query);
+
+	    $this->success("success",$ret);
 	}
 
 }
